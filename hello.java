503f3a1ed34ae8f65a3d59d7910014f48fca9170
Sharad <sharadkumars035@gmail.com>
2025-06-09 10:46:21 +0530

    Work program

--- hello.java
+++ hello.java
@@ -2,5 +2,7 @@ class hello{
     public static void main(String[] args) {
         System.out.print("hello World");
         System.out.println("First Program");
+        System.out.println("Adding something for Checkout");
+        System.out.println("Adding something for Checkout2");
     }
 }
